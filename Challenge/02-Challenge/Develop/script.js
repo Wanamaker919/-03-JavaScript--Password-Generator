39d33d823059ca7990bc4a37278e36fde78a0f72
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-04-16 14:04:12 -0400

    working on passord

--- Challenge/02-Challenge/Develop/script.js
+++ Challenge/02-Challenge/Develop/script.js
@@ -3,12 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+const characters = (alpha, numbers, symbols);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    var length = 8,
+        retVal = "";
+    for (var i = 0, n = characters.length; i < length; ++i) {
+        retVal += characters.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
 }
+
+
+ return passwordText.value = password;
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
