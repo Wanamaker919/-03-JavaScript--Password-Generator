70be10d7c99d32a88309927380bca1de8d0bb98d
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-04-26 19:46:16 -0400

    new changes

--- Challenge/02-Challenge/Develop/script.js
+++ Challenge/02-Challenge/Develop/script.js
@@ -1,31 +1,27 @@
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
-// all potential variable options
-var numbers = ['0123456789'];
-var spec = ['!@#$%^&*()_+<>?'];
-var lowerCase = ['abcdefghijklmnopqurstuvwxyz',];
-var upperCase = ['ABCDEFGHIJKLMNOPQURSTUVWXYZ',];
-// We have to prompt the user with questions about which variables they want to include in their password.
+let numbers = ['0123456789'];
+let spec = ['!@#$%^&*()_+<>?'];
+let lowerCase = ['abcdefghijklmnopqurstuvwxyz',];
+let upperCase = ['ABCDEFGHIJKLMNOPQURSTUVWXYZ',];
 function generatePassword() {
-  var passwordLength = parseInt(prompt("How many character values between 8 and 128"));
+  let passwordLength = parseInt(prompt("How many character values between 8 and 128"));
   if (passwordLength > 7 && passwordLength < 129) {
     console.log('Great!')
     } else {
       alert('please select a number between 8 and 128')
-      return ''      // needed this to return a string instead of undefined when the if statement was false
+      return ''      
     }
 
-// puts variables into the for loop
+   let hasUp = confirm("Would you like UPPERCASE letters in your password?");
+   let hasLow = confirm("Would you like lowercase letters in your password?");
+   let hasNum = confirm("Would you like numbers in your password?");
+   let hasSpec = confirm("Would you like special characters in your password?");
 
- var hasNum = confirm("Would you like numbers in your password?");
- var hasSpec = confirm("Would you like special characters in your password?");
- var hasLow = confirm("Would you like lowercase letters in your password?");
- var hasUp = confirm("Would you like UPPERCASE letters in your password?");
-// selection will be all the arrays
-  var selection = [];
-  var result = '';
- //confirmation questions to concat with variables
+  let selection = [];
+  let result = '';
+
   if (hasNum === true) {
     selection = selection.concat(numbers)
   } if (hasSpec === true) {
@@ -41,16 +37,12 @@ function generatePassword() {
     randomChar = selection[randomIn] 
     result = result + randomChar
       }
-// create a for loop for the length of the password. 
  console.log(hasLow, hasNum, hasSpec, hasUp, length);
   return result;  
 }
-// need to make if statements telling me whether or not to use certain arrays- if true selections go into the selection array to be selected
-// the math.floor(math.random) has to be run after all variables are selected.
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
